b6b69dbfdbb1eca1fc6c0d67595ffa3e21ca8966
Gugle <administration@ztxy666.cn>
2025-09-13 16:23:53 +0800

    refactor(ping): 重构客户端 Ping 处理逻辑
    -将 Ping 处理逻辑移至 MiddleKeyPingClient 类中的 handle 方法
    - 优化了 PingPayload 类中的 clientHandler 方法，使用新的 handle 方法处理 Ping
    - 删除了 PingPayload 类中冗余的代码和未使用的导入

--- src/main/java/dev/anvilcraft/ping/client/MiddleKeyPingClient.java
+++ src/main/java/dev/anvilcraft/ping/client/MiddleKeyPingClient.java
@@ -3,16 +3,40 @@ package dev.anvilcraft.ping.client;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.MultimapBuilder;
 import dev.anvilcraft.ping.MiddleKeyPing;
+import dev.anvilcraft.ping.network.PingPayload;
 import dev.anvilcraft.ping.util.Ping;
+import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.level.Level;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
+
+import java.util.Collection;
 
 @Mod(value = MiddleKeyPing.MOD_ID, dist = Dist.CLIENT)
 public class MiddleKeyPingClient {
     public static final Multimap<ResourceKey<Level>, Ping> PINGS = MultimapBuilder.hashKeys().arrayListValues().build();
 
     public MiddleKeyPingClient() {
+    }
+
+    public static void handle(PingPayload payload){
+        final Minecraft client = Minecraft.getInstance();
+        if (client.level == null) return;
+        long gameTime = client.level.getGameTime();
+        Collection<Ping> pings = MiddleKeyPingClient.PINGS.get(payload.dimension());
+        if (pings.size() > 5) pings.remove(pings.stream().findFirst().get());
+        MiddleKeyPingClient.PINGS.put(payload.dimension(), new Ping(payload, gameTime + 300));
+        SoundEvent event = switch (payload.pingType()) {
+            case GOTO -> SoundEvents.NOTE_BLOCK_PLING.value();
+            case WARNING -> SoundEvents.NOTE_BLOCK_CHIME.value();
+            case ENEMY -> SoundEvents.NOTE_BLOCK_BIT.value();
+            default -> SoundEvents.NOTE_BLOCK_BELL.value();
+        };
+        //noinspection DataFlowIssue
+        client.level.playLocalSound(client.player, event, SoundSource.PLAYERS, 1.0F, 1.0F);
     }
 }
--- src/main/java/dev/anvilcraft/ping/network/PingPayload.java
+++ src/main/java/dev/anvilcraft/ping/network/PingPayload.java
@@ -3,11 +3,9 @@ package dev.anvilcraft.ping.network;
 import dev.anvilcraft.ping.MiddleKeyPing;
 import dev.anvilcraft.ping.client.MiddleKeyPingClient;
 import dev.anvilcraft.ping.util.ByteBufCodecUtils;
-import dev.anvilcraft.ping.util.Ping;
 import dev.anvilcraft.ping.util.PingType;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.ChatFormatting;
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.codec.StreamCodec;
@@ -15,9 +13,6 @@ import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.scores.PlayerTeam;
@@ -25,7 +20,6 @@ import net.neoforged.neoforge.network.handling.DirectionalPayloadHandler;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import net.neoforged.neoforge.network.handling.IPayloadHandler;
 
-import java.util.Collection;
 import java.util.List;
 
 public record PingPayload(ResourceKey<Level> dimension, Vec3 position, PingType pingType) implements CustomPacketPayload {
@@ -52,22 +46,7 @@ public record PingPayload(ResourceKey<Level> dimension, Vec3 position, PingType 
     }
 
     public void clientHandler(IPayloadContext context) {
-        final Minecraft client = Minecraft.getInstance();
-        context.enqueueWork(() -> {
-            if (client.level == null) return;
-            long gameTime = client.level.getGameTime();
-            Collection<Ping> pings = MiddleKeyPingClient.PINGS.get(this.dimension);
-            if (pings.size() > 5) pings.remove(pings.stream().findFirst().get());
-            MiddleKeyPingClient.PINGS.put(this.dimension, new Ping(this, gameTime + 300));
-            SoundEvent event = switch (this.pingType) {
-                case GOTO -> SoundEvents.NOTE_BLOCK_PLING.value();
-                case WARNING -> SoundEvents.NOTE_BLOCK_CHIME.value();
-                case ENEMY -> SoundEvents.NOTE_BLOCK_BIT.value();
-                default -> SoundEvents.NOTE_BLOCK_BELL.value();
-            };
-            //noinspection DataFlowIssue
-            client.level.playLocalSound(client.player, event, SoundSource.PLAYERS, 1.0F, 1.0F);
-        });
+        context.enqueueWork(() -> MiddleKeyPingClient.handle(this));
     }
 
     @SuppressWarnings("resource")
