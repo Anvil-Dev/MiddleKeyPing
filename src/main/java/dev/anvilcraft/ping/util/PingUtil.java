2013a6496315925dfeb73d32b691dc9c79c6aab4
Gugle <administration@ztxy666.cn>
2026-04-17 17:37:24 +0800

    feat(ping): add max ping count configuration and update related logic

--- src/main/java/dev/anvilcraft/ping/util/PingUtil.java
+++ src/main/java/dev/anvilcraft/ping/util/PingUtil.java
@@ -100,6 +100,7 @@ public class PingUtil {
             holdUniformPingKeyWasDown = true;
         }
         MiddleKeyPingClient.PINGS.removeIf(ping -> ping == null || ping.endTime() < gameTime);
+        if(PingUtil.hasScreen()) return;
         PingType key = null;
         if (GENERIC_PING_KEY.consumeClick()) {
             key = PingType.GENERIC;
@@ -138,12 +139,6 @@ public class PingUtil {
 
     private static void processPress(Minecraft client, int action) {
         if (client.level == null) return;
-        if (action == GLFW.GLFW_PRESS) {
-            if (!holdUniformPingKeyWasDown) {
-                holdUniformPingKeyTime = client.level.getGameTime();
-            }
-            return;
-        }
         if (action == GLFW.GLFW_RELEASE) {
             if (holdUniformPingKeyWasDown) {
                 CONTROLLER.onHoldKeyReleased();
@@ -152,7 +147,18 @@ public class PingUtil {
             }
             holdUniformPingKeyWasDown = false;
             holdUniformPingKeyTime = -1L;
+            return;
         }
+        if(PingUtil.hasScreen()) return;
+        if (action == GLFW.GLFW_PRESS) {
+            if (!holdUniformPingKeyWasDown) {
+                holdUniformPingKeyTime = client.level.getGameTime();
+            }
+        }
+    }
+
+    private static boolean hasScreen() {
+        return Minecraft.getInstance().screen != null;
     }
 
     public static void sendPing(PingType key) {
