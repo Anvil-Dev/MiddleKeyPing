d4973d9fb4b1ee5a3f463d8472f3bb2421478dbd
Gugle <administration@ztxy666.cn>
2026-05-21 21:11:39 +0800

    fix(ModNetworks): make payload registrar optional during initialization

--- src/main/java/dev/anvilcraft/ping/network/ModNetworks.java
+++ src/main/java/dev/anvilcraft/ping/network/ModNetworks.java
@@ -11,7 +11,7 @@ import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 public class ModNetworks {
     @SubscribeEvent
     public static void init(RegisterPayloadHandlersEvent event) {
-        PayloadRegistrar registrar = event.registrar("1");
+        PayloadRegistrar registrar = event.registrar("1").optional();
         NetworkRegistrar.register(registrar, MiddleKeyPing.MOD_ID);
     }
 }
